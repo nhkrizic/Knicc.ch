c4a10cf74113fb3e6273aadc0700192fbfa090f3
nhkrizic <nhkrizic@outlook.cok>
2024-01-22 16:47:56 +0100

    v 0.1
    v0.1

---
+++ js/main.js
@@ -0,0 +1,162 @@
+/*  ---------------------------------------------------
+    Template Name: Dreams
+    Description: Dreams wedding template
+    Author: Colorib
+    Author URI: https://colorlib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Portfolio filter
+        --------------------*/
+        $('.portfolio__filter li').on('click', function () {
+            $('.portfolio__filter li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.portfolio__gallery').length > 0) {
+            var containerEl = document.querySelector('.portfolio__gallery');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Masonary
+    $('.work__gallery').masonry({
+        itemSelector: '.work__item',
+        columnWidth: '.grid-sizer',
+        gutter: 10
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+		Hero Slider
+	--------------------*/
+    $('.hero__slider').owlCarousel({
+        loop: true,
+        dots: true,
+        mouseDrag: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items: 1,
+        margin: 0,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+    });
+
+    var dot = $('.hero__slider .owl-dot');
+    dot.each(function () {
+        var index = $(this).index() + 1;
+        if (index < 10) {
+            $(this).html('0').append(index);
+        } else {
+            $(this).html(index);
+        }
+    });
+
+    /*------------------
+        Clipcoar copy 
+    --------------------*/
+//the helper function
+let createCopy = function(textToCopy, triggerElementId, callback = null) {
+    //add event listner to elementtrigger
+    let trigger = document.getElementById(triggerElementId);
+    trigger.addEventListener("click", function() {
+      //create the readonly textarea with the text in it and hide it
+      let tarea = document.createElement("textarea");
+      tarea.setAttribute("id", triggerElementId + "-copyarea");
+      tarea.setAttribute("readonly", "readonly");
+      tarea.setAttribute(
+        "style",
+        "opacity: 0; position: absolute; z-index: -1; top: 0; left: -9999px;"
+      );
+      tarea.appendChild(document.createTextNode(textToCopy));
+      document.body.appendChild(tarea);
+  
+      //select and copy the text in the readonly text area
+      tarea.select();
+      document.execCommand("copy");
+  
+      //remove the element from the DOM
+      document.body.removeChild(tarea);
+  
+      //fire callback function if provided
+      if (typeof callback === "function" && callback()) {
+        callback();
+      }
+    });
+  };
+  
+  
+  //usage example
+  createCopy('Sample text to copy', 'elementId', function () {
+      alert('A callback function to show your text was copied successfully!');
+  });
+  
+    /*------------------
+        Latest Slider
+    --------------------*/
+    $(".latest__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        dotsEach: 2,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 3
+            },
+            768: {
+                items: 2
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
+
+    /*------------------
+        Counter
+    --------------------*/
+    $('.counter_num').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+})(jQuery);
